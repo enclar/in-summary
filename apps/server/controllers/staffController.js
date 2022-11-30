524d464420d6401161a135a5095ff31d0aea2062
enclar <107608010+enclar@users.noreply.github.com>
2022-11-30 18:54:19 +0800

    server-feat: added in seed route for staff and tested

--- apps/server/controllers/staffController.js
+++ apps/server/controllers/staffController.js
@@ -1,17 +1,34 @@
 // Dependancies
 const express = require("express");
+const bcrypt = require("bcrypt");
+
 const Staff = require("../models/Staff");
+const seedStaff = require("../seed-data/seedStaff");
+
+// Variables
 const router = express.Router();
+const saltRounds = 10;
 
 // Routes
 // test route
 router.get("/test", (req, res) => {
-    res.json({ msg: "test route for account controller"})
+    res.json({ msg: "test route for staff controller"})
 });
 
 // seed route
 router.get("/seed", async (req, res) => {
-    await Account.deleteMany();
+    await Staff.deleteMany();
+    
+    const encryptedUsers = seedStaff.map((staff) => (
+        {
+            ...staff,
+            password: bcrypt.hashSync(staff.password, saltRounds)
+        }
+    ));
+
+    const staff = await Staff.insertMany(encryptedUsers);
+
+    res.status(201).json(staff);
 });
 
 // Export
