139e28d67cfcc4cf3c9adaa86a9baaa1528aa728
enclar <107608010+enclar@users.noreply.github.com>
2022-12-06 01:36:27 +0800

    sqlserver-feat: reconfigured schemas

--- apps/client/src/pages/NewUser.jsx
+++ apps/client/src/pages/NewUser.jsx
@@ -11,7 +11,7 @@ const NewUser = () => {
             className="w-4/5 py-20 flex flex-col items-center"
         >
             <div className="px-10 py-2">
-                <p className="text-sky-900 text-xl text-center">add a new user</p>
+                <p className="text-sky-900 text-xl text-center">add a new contact</p>
                 <div className="mt-5 flex gap-4">
                     <label className="flex gap-2">
                         <input type="radio" name="acc-type" value="staff" onClick={() => setAccType("staff")} defaultChecked />
