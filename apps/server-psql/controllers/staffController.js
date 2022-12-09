63d39fec6a538f1bef4cd9cb4f2c9b8f3db92e44
enclar <107608010+enclar@users.noreply.github.com>
2022-12-09 19:06:13 +0800

    sqlserver-feat: wrote route to add new client and successfully tested in insomnia

--- apps/server-psql/controllers/staffController.js
+++ apps/server-psql/controllers/staffController.js
@@ -1,6 +1,7 @@
 // Dependancies
 const router = require("express").Router();
 const authorization = require("../middleware/authorization");
+const bcrypt = require("bcrypt");
 
 // Prisma
 const { PrismaClient } = require("@prisma/client");
@@ -20,6 +21,26 @@ router.get("/all", authorization, async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error });
     }
-})
+});
+
+// create a new staff account
+router.post("/new", authorization, async (req, res) => {
+    try {
+        const staff = await prisma.staff.create({
+            data: {
+                ...req.body,
+                password: bcrypt.hashSync(req.body.password, 10)
+            }
+        });
+
+        if (!staff) {
+            res.status(401).json({ error: "Unable to create a new staff account" });
+        } else {
+            res.status(201).json(staff);
+        }
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
 
 module.exports = router;
