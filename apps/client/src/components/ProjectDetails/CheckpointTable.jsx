84a4ebb33ef10a890926424edf98ba794bc7bc6b
enclar <107608010+enclar@users.noreply.github.com>
2022-12-10 15:54:52 +0800

    client-feat: linked checkpoint delete to server route and successfully tested

--- apps/client/src/components/ProjectDetails/CheckpointTable.jsx
+++ apps/client/src/components/ProjectDetails/CheckpointTable.jsx
@@ -5,13 +5,30 @@ const CheckpointTable = () => {
     const [checkpoints, setCheckpoints] = useAtom(checkpointAtom);
 
     // function to delete checkpoint
-    const deleteCheckpoint = async () => {
+    const deleteCheckpoint = async (checkpoint_id) => {
+        const url = "/api/checkpoints/delete/" + checkpoint_id
         try {
+            const response = await fetch(url, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json',
+                    token: JSON.parse(localStorage.getItem("token"))
+                }
+            });
 
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log("deleted checkpoint:", data);
+                const filtered = checkpoints?.filter((checkpoint) => checkpoint.id !== data.id);
+                setCheckpoints(filtered);
+            } else {
+                console.log("server error:", data.error);
+            }
         } catch (error) {
             console.log("client error:", error)
         }
-    }
+    };
 
     return (
         <table>
@@ -28,7 +45,10 @@ const CheckpointTable = () => {
                             <tr key={index}>
                                 <td className="px-5 border-2 text-center">{checkpoint?.date?.slice(0, 10)}</td>
                                 <td className="px-5 border-2">{checkpoint?.details}</td>
-                                <td className="px-5 border-2 text-center">remove</td>
+                                <td
+                                    onClick={() => deleteCheckpoint(checkpoint.id)}
+                                    className="px-5 border-2 text-center hover:cursor-pointer hover:underline"
+                                >remove</td>
                             </tr>
                         )
                     })
